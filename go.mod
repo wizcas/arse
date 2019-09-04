e614a3acf102751a7565dac754d0d7a114b32041
Wizcas Chen <chen@0x1c.dev>
2019-09-05 00:31:55 +0800

    add cli interaction

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/wizcas/arse
 
 go 1.13
 
-require gopkg.in/yaml.v2 v2.2.2
+require (
+	github.com/urfave/cli v1.21.0
+	gopkg.in/yaml.v2 v2.2.2
+)
--- main.go
+++ main.go
@@ -2,13 +2,44 @@ package main
 
 import (
 	"fmt"
+	"github.com/urfave/cli"
 	"github.com/wizcas/arse/parser"
 	"log"
+	"os"
 )
 
 func main() {
+	app := cli.NewApp()
+	app.Name = "arse"
+	app.Usage = "Run custom actions with your own config"
+	app.Commands = []cli.Command{
+		{
+			Name:      "run",
+			ShortName: "r",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "config, c",
+					Usage: "Specify the config file instead of './.arse.yml'",
+				},
+			},
+			Action: func(c *cli.Context) error {
+				configFile := c.String("config")
+				fmt.Printf("running with file: %s\n", configFile)
+				run(configFile)
+				return nil
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func run(filename string) {
 	println("Hello my arse!")
-	data, err := parser.Load("./tests/test_config.yml")
+	data, err := parser.Load(filename)
 	if err != nil {
 		log.Fatal(err)
 	}

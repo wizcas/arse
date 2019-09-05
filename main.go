d7c040ce7d49e0b4577e4537f6469a3c09d723f0
Wizcas Chen <chen@wizcas.me>
2019-09-05 16:20:55 +0800

    refactored the loader structure and changed the CLI entrance

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"os"
 
-	"github.com/wizcas/arse/loaders/yaml"
+	"github.com/wizcas/arse/loader"
 
 	"github.com/urfave/cli"
 )
@@ -18,22 +18,39 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "arse"
 	app.Usage = "Run custom actions with your own config"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "config, c",
+			Usage: "Specify the config file instead of './.arse.yml'",
+		},
+	}
+	app.Action = run
 	app.Commands = []cli.Command{
 		{
 			Name:      "run",
-			ShortName: "r",
+			Aliases:   []string{"r"},
+			Usage:     `Run a configured action by name`,
+			UsageText: "arse [run|r] <ACTION_NAME>",
+			Description: `This command finds the action specified by ACTION_NAME and execute it.
+	 It's also the default command so you can omit the 'run' part.
+	 For example, the following 2 commands:
+		arse hello
+		arse run hello
+	 both run an action named 'hello' which is defined in the config file.`,
+			Action: run,
+		},
+		{
+			Name:     "init",
+			Category: "Utilities",
+			Usage:    "Create a new arse config file of chosen type with a bootstrap template",
 			Flags: []cli.Flag{
 				cli.StringFlag{
-					Name:  "config, c",
-					Usage: "Specify the config file instead of './.arse.yml'",
+					Name:  "type, t",
+					Usage: "The type of arse config file",
 				},
 			},
 			Action: func(c *cli.Context) error {
-				configFile := c.String("config")
-				if len(configFile) == 0 {
-					configFile = defaultConfigFile
-				}
-				run(configFile)
+				fmt.Println("Sorry but I haven't done it yet.")
 				return nil
 			},
 		},
@@ -44,11 +61,16 @@ func main() {
 	}
 }
 
-func run(filename string) {
+func run(c *cli.Context) error {
+	configFile := c.String("config")
+	if len(configFile) == 0 {
+		configFile = defaultConfigFile
+	}
 	println("Hello my arse!")
-	data, err := yaml.Load(filename)
+	data, err := loader.SmartLoad(configFile)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Printf("Loaded ArseFile:\n%v\n", data)
+	return nil
 }
